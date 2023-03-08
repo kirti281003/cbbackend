466bfc27d2c6a2b348ef3495a168fe7a55e863e0
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2023-03-08 03:02:38 -0800

    logout fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,6 @@ const{isAuthenticated,authorizeRoles}=require("../middleware/auth");
 const router=express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").get(logout);
+router.route("/logout").post(logout);
 router.route("/user").get(isAuthenticated,getUser);
 module.exports=router;
