ef09aefb145a7af445d3fd66fbc41b53d6281f5e
Kirti Aggarwal <kirtiaggarwal@Kirtis-MacBook-Air.local>
2024-12-05 07:10:14 +0530

    updating cors

--- app.js
+++ app.js
@@ -19,11 +19,24 @@ app.use("/api/v1",user);
 app.use("/api/v1",post);
 // app.use("/api/v1",sub);
 app.use(errorMiddleware);
-app.use(function (request, response, next) {
-    response.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
-    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-response.setHeader("Access-Control-Allow-Credentials", true);
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://localhost:3000"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
     next();
   });
   app.use(cors());
